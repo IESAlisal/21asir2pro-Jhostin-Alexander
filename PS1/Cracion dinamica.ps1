56657ece8e068af9b5967da26a95b280789997d0
Jhostin-Alexander <91968564+Jhostin-Alexander@users.noreply.github.com>
2022-05-03 22:28:25 +0200

    Update Cracion dinamica.ps1

--- PS1/Cracion dinamica.ps1
+++ PS1/Cracion dinamica.ps1
@@ -1,6 +1,16 @@
 ﻿##Jhostin Alexander Cadme Salinas##
-#Creacion masiva de usuarios en unidades organizativas y sus
-##respectivos grupos ##
+##Funcionalidades:##
+##Este script nos permite indicar nombre de usuarios y la cantidad que deseamos crear##
+##NoS preguntara graficamente si deseamos crear una nueva unidad organizativa##
+##En funcion de nuestra respuesta Ejecutara una funcion u otra al tratarse de un condicional##
+##CASO SI##
+##Nos preguntar nombre la nueva UO y la creara##
+##Nos preguntara la contraseña predeterminada para los nuevos usuarios que crearemos en la nueva OU##
+##Creara los usuarios indicados con el nombre que nos habia preguntado anteriormente y con su cantidad indicada y la contraseña##
+##Todo este en la UO indica y los metera en en un grupo con sus nombre respectivos##
+##CASO NO##
+##Los pasos seran igual que el anterior pero indicaremos una UO que ya este creada anteriormente##
+
 
 cls
 
@@ -10,35 +20,35 @@ foreach($usuario in $usuarios){
         $UnidadO=Read-Host "¿Desea crear unidad organizativa nueva?(SI/NO)"
     if($UnidadO -eq "Si"){
         $NuevaOU=Read-host "Indique Nombre De La Nueva OU"
-        Write-host "Se crearan los usuarios en la OU indicada"
-        Write-host "La contraseña predeterminada debera cumplir requisitos de complejidad"
+            Write-host "Se crearan los usuarios en la OU indicada"
+            Write-host "La contraseña predeterminada debera cumplir requisitos de complejidad"
         $contraseña=Read-Host "Indique contraseña predeterminada de los usuarios"
-        write-host "Se añadiran los usuarios a su grupo correspondiente."
-        New-ADOrganizationalUnit -Name $NuevaOU -ProtectedFromAccidentalDeletion:$false
-        New-ADGroup -name $usuario -Path "OU=$NuevaOU,DC=Proyecto,DC=es" -GroupScope DomainLocal
+            write-host "Se añadiran los usuarios a su grupo correspondiente."
+            New-ADOrganizationalUnit -Name $NuevaOU -ProtectedFromAccidentalDeletion:$false
+            New-ADGroup -name $usuario -Path "OU=$NuevaOU,DC=Proyecto,DC=es" -GroupScope DomainLocal
     for($i=1;$i -le $cantidad;$i++){
 
         $contra=ConvertTo-SecureString -string $contraseña -Asplaintext -Force 
-        #Remove-ADUser -Identity $usuario$i -Confirm:$false
-        New-ADUser -Name $usuario$i -AccountPassword $contra -Path "OU=$NuevaOU,DC=Proyecto,DC=es" -passthru -Enabled $true -ChangePasswordAtLogon $true
+            #Remove-ADUser -Identity $usuario$i -Confirm:$false
+            New-ADUser -Name $usuario$i -AccountPassword $contra -Path "OU=$NuevaOU,DC=Proyecto,DC=es" -passthru -Enabled $true -ChangePasswordAtLogon $true
 
-        Add-ADGroupMember -Identity $usuario -Members $usuario$i
+            Add-ADGroupMember -Identity $usuario -Members $usuario$i
         }
     }
     if($UnidadO -eq "No"){
         $UO=Read-host "Indique la Unidad Organizativa existente"
-        Write-host "Se crearan los usuarios en la OU indicada"
-        Write-host "La contraseña predeterminada debera cumplir requisitos de complejidad"
+            Write-host "Se crearan los usuarios en la OU indicada"
+            Write-host "La contraseña predeterminada debera cumplir requisitos de complejidad"
         $contraseña=Read-Host "Indique contraseña predeterminada de los usuarios"
-        write-host "Se añadiran los usuarios a su grupo correspondiente."
-        New-ADGroup -name $usuario -Path "OU=$UO,DC=Proyecto,DC=es" -GroupScope DomainLocal
+            write-host "Se añadiran los usuarios a su grupo correspondiente."
+            New-ADGroup -name $usuario -Path "OU=$UO,DC=Proyecto,DC=es" -GroupScope DomainLocal
     for($i=1;$i -le $cantidad;$i++){
 
         $contra=ConvertTo-SecureString -string $contraseña -Asplaintext -Force 
-        #Remove-ADUser -Identity $usuario$i -Confirm:$false
-        New-ADUser -Name $usuario$i -AccountPassword $contra -Path "OU=$UO,DC=Proyecto,DC=es" -passthru -Enabled $true -ChangePasswordAtLogon $true
+            #Remove-ADUser -Identity $usuario$i -Confirm:$false
+            New-ADUser -Name $usuario$i -AccountPassword $contra -Path "OU=$UO,DC=Proyecto,DC=es" -passthru -Enabled $true -ChangePasswordAtLogon $true
 
-        Add-ADGroupMember -Identity $usuario -Members $usuario$i
+            Add-ADGroupMember -Identity $usuario -Members $usuario$i
         }
     }
 }
