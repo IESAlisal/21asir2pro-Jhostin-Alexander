5d09a1a18a3102d49757526b76eeda14f5a94a75
Jhostin-Alexander <91968564+Jhostin-Alexander@users.noreply.github.com>
2022-05-03 22:04:35 +0200

    Update Scripts

---
+++ PS1/Cracion dinamica.ps1
@@ -0,0 +1,53 @@
+﻿##Jhostin Alexander Cadme Salinas##
+#Creacion masiva de usuarios en unidades organizativas y sus
+##respectivos grupos ##
+
+cls
+
+$usuarios=Read-host "Indique nombre de usuarios que desee crear"; ##usuarios que crearemos##
+$cantidad=Read-host "Indique la cantidad de usuarios";
+foreach($usuario in $usuarios){
+        $UnidadO=Read-Host "¿Desea crear unidad organizativa nueva?(SI/NO)"
+    if($UnidadO -eq "Si"){
+        $NuevaOU=Read-host "Indique Nombre De La Nueva OU"
+        Write-host "Se crearan los usuarios en la OU indicada"
+        Write-host "La contraseña predeterminada debera cumplir requisitos de complejidad"
+        $contraseña=Read-Host "Indique contraseña predeterminada de los usuarios"
+        write-host "Se añadiran los usuarios a su grupo correspondiente."
+        New-ADOrganizationalUnit -Name $NuevaOU -ProtectedFromAccidentalDeletion:$false
+        New-ADGroup -name $usuario -Path "OU=$NuevaOU,DC=Proyecto,DC=es" -GroupScope DomainLocal
+    for($i=1;$i -le $cantidad;$i++){
+
+        $contra=ConvertTo-SecureString -string $contraseña -Asplaintext -Force 
+        #Remove-ADUser -Identity $usuario$i -Confirm:$false
+        New-ADUser -Name $usuario$i -AccountPassword $contra -Path "OU=$NuevaOU,DC=Proyecto,DC=es" -passthru -Enabled $true -ChangePasswordAtLogon $true
+
+        Add-ADGroupMember -Identity $usuario -Members $usuario$i
+        }
+    }
+    if($UnidadO -eq "No"){
+        $UO=Read-host "Indique la Unidad Organizativa existente"
+        Write-host "Se crearan los usuarios en la OU indicada"
+        Write-host "La contraseña predeterminada debera cumplir requisitos de complejidad"
+        $contraseña=Read-Host "Indique contraseña predeterminada de los usuarios"
+        write-host "Se añadiran los usuarios a su grupo correspondiente."
+        New-ADGroup -name $usuario -Path "OU=$UO,DC=Proyecto,DC=es" -GroupScope DomainLocal
+    for($i=1;$i -le $cantidad;$i++){
+
+        $contra=ConvertTo-SecureString -string $contraseña -Asplaintext -Force 
+        #Remove-ADUser -Identity $usuario$i -Confirm:$false
+        New-ADUser -Name $usuario$i -AccountPassword $contra -Path "OU=$UO,DC=Proyecto,DC=es" -passthru -Enabled $true -ChangePasswordAtLogon $true
+
+        Add-ADGroupMember -Identity $usuario -Members $usuario$i
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
--- PS1/CreaUO y cuentas de usuario.ps1
+++ PS1/CreaUO y cuentas de usuario.ps1
@@ -1,11 +1,21 @@
-﻿cls
-$usuarios="Inbound","Outbound";
+﻿##Jhostin Alexander Cadme Salinas##
+#Creacion masiva de usuarios en unidades organizatias y sus
+##respectivos grupos ##
+
+cls
+$usuarios="Inbound","Outbound"; ##usuarios que crearemos##
 foreach($usuario in $usuarios){
-for($i=1;$i -le 10;$i++){
-#New-ADOrganizationalUnit -Name "Agentes" 
+for($i=1;$i -le 100;$i++){
+#New-ADOrganizationalUnit -Name "Agentes" ## Unidad Organizativa##
 #write-host $usuario $i
 $contra=ConvertTo-SecureString -string "Hola1***" -Asplaintext -Force 
-#Remove-ADUser -Identity $usuario$i 
+#Remove-ADUser -Identity $usuario$i -Confirm:$false
 New-ADUser -Name $usuario$i -AccountPassword $contra -Path "OU=Agentes,DC=Proyecto,DC=es" -passthru -Enabled $true -ChangePasswordAtLogon $true
+#Remove-adgroup -Identity $usuario -Confirm:$false
+New-ADGroup -Name $usuario -GroupScope DomainLocal -Path "OU=Agentes,DC=Proyecto,DC=es"
+Add-ADGroupMember -identity $usuario -Members $usuario$i
+
+
 }
 }
+
